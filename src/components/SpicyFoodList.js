6c5891f67823f8e6b20a8a45871662728a9e0eb9
prince475 <danielmwai475@gmail.com>
2023-01-17 13:50:26 +0300

    working with arrays and filters

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -5,12 +5,28 @@ function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
   function handleAddFood() {
-    const newFood = getNewRandomSpicyFood();
+    const newFood = getNewRandomSpicyFood();//adding newly generated food returned
+    //using spread operator to create a copy of our foods array and insert it it into anew array
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
     console.log(newFood);
   }
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+const [filterBy, setFilterBy] = useState("All");
+
+const foodsToDisplay = foods.filter((food) => {
+  if (filterBy === "All") {
+    return true;
+  } else {
+    return food.cuisine === filterBy;
+  }
+});
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -19,6 +35,13 @@ function SpicyFoodList() {
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      <select name="filter" onChange={handleFilterChange}>
+          <option value="All">All</option>
+          <option value="American">American</option>
+          <option value="Sichuan">Sichuan</option>
+          <option value="Thai">Thai</option>
+          <option value="Mexican">Mexican</option>
+      </select>
     </div>
   );
 }
